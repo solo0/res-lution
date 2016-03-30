a6d50655685395385b0f44169fc195c14b437ed7
solo0 <dmytro.orlyk@google.com>
2016-03-30 16:07:09 +0300

    beforepackages

--- resolutions.js
+++ resolutions.js
@@ -3,7 +3,15 @@ Resolutions = new Mongo.Collection('resolutions');
 if (Meteor.isClient) {
   Template.body.helpers({
     resolutions: function() {
+      if (Session.get('hideFinished')){
+        return Resolutions.find({checked: {$ne: true}});
+      } else{
       return Resolutions.find();
+    }
+  },
+    hideFinished: function(){
+
+      return Session.get('hideFinished');
     }
   });
 
@@ -17,9 +25,19 @@ if (Meteor.isClient) {
       });
       event.target.title.value = "";
       return false;
-      
-    }
+    },
+    'change .hide-finished': function(event){
+      Session.set('hideFinished',event.target.checked);
 
+    }
+  });
+  Template.resolution.events({
+    'click .toggle-checked': function(){
+      Resolutions.update(this._id, {$set: {checked: !this.checked}});
+    },
+    'click .delete': function(){
+      Resolutions.remove(this._id);
+    }
   });
 }
 
